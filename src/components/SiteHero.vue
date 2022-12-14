8e983f8a079ca94403eebbeb556436c334ffb5bd
[leanne-yu] <[leanneyu3299@gmail.com]>
2022-12-14 11:59:56 -0500

    Fixed green movie images.

--- src/components/SiteHero.vue
+++ src/components/SiteHero.vue
@@ -5,10 +5,23 @@
 <template>
     <div class="image1">
         <img src="https://i.imgur.com/eBEkqgn.jpg" width="600" height="500"/>
+        <img src="https://i.imgur.com/eBEkqgn.jpg" width="600" height="500"/>
+    </div>
+    <div class="image2">
+        <img src="https://i.imgur.com/eBEkqgn.jpg" width="600" height="500"/>
+        <img src="https://i.imgur.com/eBEkqgn.jpg" width="600" height="500"/>
+    </div>
+        <!-- <img src="https://i.imgur.com/eBEkqgn.jpg" width="600" height="500"/>
     </div>
     <div class="image2">
         <img src="https://i.imgur.com/eBEkqgn.jpg" width="600" height="500"/>
     </div>
+    <div class="image3">
+        <img src="https://i.imgur.com/eBEkqgn.jpg" width="600" height="500"/>
+    </div>
+    <div class="image4">
+        <img src="https://i.imgur.com/eBEkqgn.jpg" width="600" height="500"/>
+    </div> -->
 
 <h2>Shrek has the best movies, just for you! Subscribe for only $39.99 a month and join now and get 0 days FREE :D ahahaha this is the best website ever.</h2>
 </template>
@@ -19,7 +32,6 @@
     position: relative;
     padding: 0%;
     margin: 0px;
-    width: 50%;
 }
 
 h2 {
@@ -40,9 +52,8 @@ h2 {
     border-radius: 12px;
 }
 
-.image2{
-    display: flex;
-    margin-top: 0%;
-    float: right;
+.image2 {
+    display: flex
+
 }
 </style>
